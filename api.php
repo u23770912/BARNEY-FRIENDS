740c84a44621fb9d8cfcd421b469f1345970feaa
u23770912 <u23770912@tuks.co.za>
2025-05-24 10:33:04 +0200

    loginfix2

--- api.php
+++ api.php
@@ -2,9 +2,9 @@
 <?php
 session_start();
 //error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
-//ini_set('display_errors', 1);
+ini_set('display_errors', 1);
 //ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
+error_reporting(E_ALL);
 
 header("Content-Type: application/json");
 
